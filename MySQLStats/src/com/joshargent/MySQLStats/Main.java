93d2a85cb93706b026ae0fa0c53dac4c1c23d49b
Josh Argent <josh@vanillaworld.org>
2014-05-24 17:02:49 +0100

    Added plugin.yml and added onEnable and onDisable methods

--- MySQLStats/src/com/joshargent/MySQLStats/Main.java
+++ MySQLStats/src/com/joshargent/MySQLStats/Main.java
@@ -1,5 +1,17 @@
 package com.joshargent.MySQLStats;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public void onEnable()
+	{ 
+		
+	}
+	
+	public void onDisable()
+	{
+		
+	}
 
 }
